20316c54df32c9a94357bca15fc7f21e92eb7232
Tomohiro Nishimura <tomohiro68@gmail.com>
2014-02-16 14:53:31 +0900

    implemented library

--- lib/ehon.rb
+++ lib/ehon.rb
@@ -1,5 +1,63 @@
 require "ehon/version"
 
 module Ehon
-  # Your code goes here...
+  def self.included(base)
+    base.extend ClassMethods
+    base.contents        = {}
+    base.default_options = {}
+  end
+
+  def initialize(id, options = {})
+    @options = options.merge(id: id)
+  end
+
+  def options
+    self.class.default_options.merge(@options)
+  end
+
+  def ==(other)
+    (self.class == other.class) && (self.id == other.id)
+  end
+
+  def respond_to_missing?(symbol, include_private = false)
+    self.options.has_key?(symbol)
+  end
+
+  def method_missing(symbol, *args)
+    return self.options[symbol] if respond_to?(symbol)
+    super
+  end
+
+  module ClassMethods
+    attr_accessor :contents, :default_options
+
+    def page(id, options = {}, &block)
+      instance = new(id, options)
+      instance.instance_eval(&block) if block_given?
+      self.contents[id] = instance
+      instance
+    end
+    alias enum page
+
+    def default(options = {})
+      self.default_options.merge!(options)
+    end
+
+    def all
+      self.contents.values
+    end
+
+    def find(*queries)
+      queries.flatten!
+      findeds = queries.map {|query|
+        next self.contents[query] unless query.is_a?(Hash)
+        self.contents.values.find {|instance|
+          query.all? {|key, value| instance.options[key] == value }
+        }
+      }.compact
+      queries.size == 1 ? findeds.first : findeds
+    end
+    alias [] find
+  end
 end
+
--- spec/ehon_spec.rb
+++ spec/ehon_spec.rb
@@ -2,10 +2,283 @@ require 'spec_helper'
 
 describe Ehon do
   it 'should have a version number' do
-    Ehon::VERSION.should_not be_nil
+    expect(Ehon::VERSION).to_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  subject { Item }
+
+  before :all do
+    class Item
+      include Ehon
+    end
+  end
+
+  after do
+    Item.contents = {}
+    Item.default_options = {}
+  end
+
+  describe '.page' do
+
+    shared_examples_for 'a page' do
+      it 'should create one instance' do
+        expect(subject.contents.size).to eq(1)
+      end
+
+      it 'instance id equals to argument' do
+        expect(@instance.id).to eq(expected_id)
+      end
+    end
+
+    context 'without options' do
+      3.times do
+        random_id = rand(10)
+
+        before { @instance = subject.page(expected_id) }
+
+        context "id is #{random_id}" do
+          let(:expected_id) { random_id }
+          it_behaves_like "a page"
+        end
+      end
+    end
+
+    context 'with options' do
+      before { @instance = subject.page(expected_id, expected_options) }
+
+      let(:expected_id) { 3 }
+      let(:expected_options) { {name: 'potion', value: 5} }
+
+      it_behaves_like "a page"
+
+      it 'instance options equals to argument with id: 3' do
+        expect(@instance.options).to eq(expected_options.merge(id: 3))
+      end
+
+      3.times do
+        random_key   = ('a'..'z').to_a.shuffle.take(5).join.to_sym
+        random_value = rand(100)
+
+        context "with {#{random_key}: #{random_value}}" do
+          let(:expected_options) { {random_key => random_value} }
+
+          it "responds to #{random_key}" do
+            expect(@instance).to be_respond_to(random_key)
+          end
+
+          it "instance should return #{random_value} when call #{random_key}" do
+            expect(@instance.__send__(random_key)).to eq(random_value)
+          end
+        end
+      end
+    end
+
+    context 'with default options' do
+      before do
+        class Item
+          default key: 'value'
+        end
+        @instance = subject.page(1)
+      end
+
+      it "should has default option {key: 'value'}" do
+        expect(Item.default_options).to eq({key: 'value'})
+      end
+
+      it "instacne should respond to key and return 'value'" do
+        expect(@instance.key).to eq('value')
+      end
+    end
+
+    context 'with block' do
+      before do
+        @instance_with_block = subject.page(1, name: 'potion') {
+          def potion?
+            true
+          end
+        }
+        @instance_without_block = subject.page(2, name: 'pass')
+      end
+
+      it 'instance_with_block should respond to `potion?`' do
+        expect(@instance_with_block).to be_respond_to(:potion?)
+      end
+
+      it 'instance_with_block should *not* respond to `potion?`' do
+        expect(@instance_without_block).to_not be_respond_to(:potion?)
+      end
+    end
+  end
+
+  describe '#==(other)' do
+    context 'same class' do
+      before do
+        @instance = Item.page(1)
+        @other    = @instance.dup
+      end
+      it 'instance should equal to cpy' do
+        expect(@instance).to eq(@other)
+      end
+    end
+
+    context 'same id but other class' do
+      before do
+        class Other
+          include Ehon
+        end
+        @instance = Item.page(1)
+        @other    = Other.page(1)
+      end
+      it "instance should not equal to other" do
+        expect(@instance).to_not eq(@other)
+      end
+    end
+
+    context 'with case' do
+      before do
+        class Difficulty
+          include Ehon
+
+          EASY   = enum 1, name: 'Easy mode'
+          NORMAL = enum 2, name: 'Normal mode'
+          HARD   = enum 3, name: 'Hard mode'
+        end
+        @selected = Difficulty::HARD
+      end
+
+      it 'selected mode should be hard' do
+        should satisfy {
+          case @selected
+            when Difficulty::EASY   then false
+            when Difficulty::NORMAL then false
+            when Difficulty::HARD   then true
+            else;                        false
+          end
+        }
+      end
+    end
+  end
+
+  describe '.all' do
+    before do
+      subject.page 1
+      subject.page 2
+      subject.page 3
+    end
+
+    it 'should has 3 pages' do
+      expect(subject.all.size).to eq(3)
+    end
+
+    # FIXME: it depends an implementation
+    it 'all should return contents.values' do
+      expect(subject.all).to eq(subject.contents.values)
+    end
+  end
+
+  describe '.find(id_or_query)' do
+    before do
+      subject.page 1,   name: 'potion',      value: 5
+      subject.page 2,   name: 'high potion', value: 10
+      subject.page '3', name: 'scroll'
+    end
+
+    context 'with key' do
+      it 'find with id 1 should return potion' do
+        finded = subject.find(1)
+        expect(finded.name).to eq('potion')
+      end
+
+      it "find with id '3' should return scroll" do
+        finded = subject.find('3')
+        expect(finded.name).to eq('scroll')
+      end
+
+      it 'find with id 12 should return nil' do
+        finded = subject.find(12)
+        expect(finded).to be_nil
+      end
+    end
+
+    context 'with multiple id' do
+      it 'find with id 1 and 2 should return `potion` and `high potion`' do
+        finded = subject.find(1, 2)
+        expect(finded.map(&:name)).to eq(['potion', 'high potion'])
+      end
+
+      it 'find with id 1 and 9 should return `potion` only but array' do
+        finded = subject.find(1, 9)
+        expect(finded.map(&:name)).to eq(['potion'])
+      end
+
+      it 'find with id [1, 2] should return `potion` only but array' do
+        finded = subject.find([1, 2])
+        expect(finded.map(&:name)).to eq(['potion', 'high potion'])
+      end
+
+      it 'find with id [4, 8] should return empty array' do
+        finded = subject.find([4, 8])
+        expect(finded).to eq([])
+      end
+    end
+
+    context 'with query' do
+      it "find with `{name: 'high potion'}` should return item id 2" do
+        finded = subject.find(name: 'high potion')
+        expect(finded.id).to eq(2)
+      end
+
+      it "find with `{value: 5}` should return item id 1" do
+        finded = subject.find(value: 5)
+        expect(finded.id).to eq(1)
+      end
+
+      it "find with `{id: '3'}` should return item id '3'" do
+        finded = subject.find(id: '3')
+        expect(finded.id).to eq('3')
+      end
+
+      it "find with `{name: 'food'}` should return nil" do
+        finded = subject.find(name: 'food')
+        expect(finded).to be_nil
+      end
+
+      context 'AND search' do
+        it "find with `{name: 'potion', value: 5}` should return item id 1" do
+          finded = subject.find(name: 'potion', value: 5)
+          expect(finded.id).to eq(1)
+        end
+
+        it "find with `{name: 'potion', value: 10}` should return nil" do
+          finded = subject.find(name: 'potion', value: 10)
+          expect(finded).to be_nil
+        end
+      end
+
+      context 'OR search' do
+        it "find with `[{name: 'potion'}, {value: 10}]` should return item id `potion` and `high potion`" do
+          finded = subject.find([{name: 'potion'}, {value: 10}])
+          expect(finded.map(&:name)).to eq(['potion', 'high potion'])
+        end
+
+        it "find with `{name: 'potion', value: 20}` should return potion only but array" do
+          finded = subject.find([{name: 'potion'}, {value: 20}])
+          expect(finded.map(&:name)).to eq(['potion'])
+        end
+
+        it "find with `{name: 'fire potion', value: 20}` should return empty array" do
+          finded = subject.find([{name: 'fire potion'}, {value: 20}])
+          expect(finded).to eq([])
+        end
+      end
+    end
+
+    context 'with id and query' do
+        it "find with `[1, {value: 10}]` should return item id `potion` and `high potion`" do
+          finded = subject.find([1, {value: 10}])
+          expect(finded.map(&:name)).to eq(['potion', 'high potion'])
+        end
+    end
   end
 end
+
